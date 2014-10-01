d798d585d1313c48e1d00b7c101c4fac944544be
Megan Gebhard <mgebhard1995@gmail.com>
2014-10-01 15:43:36 -0400

    Schedule Script

---
+++ project/Assets/Schedule.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Schedule : MonoBehaviour {
+	public float projectThree;
+	public float projectTwo;
+	public float projectOne;
+
+	// Use this for initialization
+	void Start () {
+		projectOne = 1f;
+		projectTwo = 1f;
+		projectThree = 1f;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		projectOne -= 0.02f;
+	
+	}
+}
