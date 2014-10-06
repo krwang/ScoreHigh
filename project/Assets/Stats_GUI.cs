1fd9144543c257b74dd07ebba6f4ab563e24b454
Sen Chang <tonych@mit.edu>
2014-10-06 15:35:32 -0400

    updated stats with text rendering

--- project/Assets/Stats_GUI.cs
+++ project/Assets/Stats_GUI.cs
@@ -6,11 +6,16 @@ public class Stats_GUI : MonoBehaviour {
 	public Stats stat;
 	public GameObject stat_background;
 	public GameObject stat_foreground;
+	public GameObject stat_text;
+	private TextMesh stat_text_mesh;
 
 	void Start () {
+		stat_text_mesh = stat_text.GetComponent<TextMesh> ();
 	}
 
 	void Update() {
 		stat_foreground.transform.localScale = new Vector3(stat.val,1,1);
+		stat_text_mesh.text = stat.stat_name + ": " + System.String.Format("{0:P}",stat.val);
+
 	}
 }
