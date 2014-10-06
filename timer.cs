ab401fb8425508d0cca9332771e3469eb05e1bce
lennymartinez <lmartinezd21@gmail.com>
2014-10-06 15:14:49 -0400

    timer updated
    timer

---
+++ timer.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class timer : MonoBehaviour {
+	float timeLeft = 30.0f;
+
+	// Use this for initialization
+	void Start () {
+	}
+
+	
+	// Update is called once per frame
+	void Update () {
+		timeLeft -= Time.deltaTime;
+		if(timeLeft < 0)
+		{
+			guiText.text = "Hello";
+		}
+
+	}
+}
