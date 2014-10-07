776e628fca83dc4eefc8760c3ef4f54eb8e20a6f
Megan Gebhard <mgebhard1995@gmail.com>
2014-10-06 23:36:25 -0400

    Added MITClass class and initialized three tasks.

--- project/Assets/Schedule.cs
+++ project/Assets/Schedule.cs
@@ -1,21 +1,57 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class Schedule : MonoBehaviour {
-	public float projectThree;
-	public float projectTwo;
-	public float projectOne;
+public class MITClass 
+{
+	public string className;
+	public string courseNumber;
+	public string task;
+	public string location;
+	public float minutesDue;
+	
+	public MITClass(string name, string number, string building, string assignment, float deadline)
+	{
+		className = name;
+		courseNumber = number;
+		location = building;
+		task = assignment;
+		minutesDue = deadline;
+	}
 
+	public void Update ()
+	{
+		minutesDue -= Time.deltaTime;
+		if(minutesDue < 0)
+		{
+			// Failed a task
+		}
+	}
+
+	public void TaskExtension(float timeExtended)
+	{
+		minutesDue += timeExtended;
+	}
+}
+
+public class Schedule : MonoBehaviour {
+	public MITClass taskOne;
+	public MITClass taskTwo;
+	public MITClass taskThree;
+	public float speed;
+	
 	// Use this for initialization
 	void Start () {
-		projectOne = 1f;
-		projectTwo = 1f;
-		projectThree = 1f;
+		taskOne = new MITClass("Algorithms", "6.006", "Green Building", "Test", 25);
+		taskTwo = new MITClass("Game Design", "6.073", "Stata", "Project 3", 25);
+		taskThree = new MITClass("Math for Computer Scientist", "6.042", "26-100", "Number Theorey Pset", 25);
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		projectOne -= 0.02f;
-	
+		taskOne.Update ();
+		taskTwo.Update ();
+		taskThree.Update ();
+		
+		
 	}
 }
