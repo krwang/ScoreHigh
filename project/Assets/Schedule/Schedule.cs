9731a7b5361ae324569376afe814554479e60631
sdrammis <sdrammis@mit.edu>
2014-10-07 23:43:51 -0400

    adding hacky way for showing class information

--- project/Assets/Schedule/Schedule.cs
+++ project/Assets/Schedule/Schedule.cs
@@ -36,18 +36,29 @@ public class MITClass
 	{
 		minutesDue += timeExtended;
 	}
+
+	public string toString() {
+		return className + " " + courseNumber + "\n"
+				+ "todo: " + task + "\n"
+				+ "location: " + location + "\n"
+				+ "minutes due: " + minutesDue.ToString("F2");
+	}
 }
 
 public class Schedule : MonoBehaviour {
 	public MITClass taskOne;
 	public MITClass taskTwo;
 	public MITClass taskThree;
+	public TextMesh classText;
 
 	// Use this for initialization
 	void Start () {
 		taskOne = new MITClass("Algorithms", "6.006", "Green Building", "Test", 25);
 		taskTwo = new MITClass("Game Design", "6.073", "Stata", "Project 3", 30);
 		taskThree = new MITClass("Math for Computer Scientist", "6.042", "26-100", "Number Theorey Pset", 55);
+
+
+		classText = this.GetComponent<TextMesh> ();
 	}
 	
 	// Update is called once per frame
@@ -56,15 +67,21 @@ public class Schedule : MonoBehaviour {
 		taskOne.runClock (Time.deltaTime);
 		taskTwo.runClock (Time.deltaTime);
 		taskThree.runClock (Time.deltaTime);
+
+
+		classText.text = taskOne.toString () + "\n\n"
+						+ taskTwo.toString () + "\n\n"
+						+ taskThree.toString ();
 	}
+
 	private bool guiIsOn = true;
 	
-	void OnGUI()
-	{
-		if(guiIsOn)
-		{
-			Debug.Log("In gui");
-			GUI.Label(new Rect(5,5,5,5), "HELPP");
-		}
-	}
+//	void OnGUI()
+//	{
+//		if(guiIsOn)
+//		{
+//			Debug.Log("In gui");
+//			GUI.Label(new Rect(5,5,5,5), "HELPP");
+//		}
+//	}
 }
