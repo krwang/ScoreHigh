d500871b4a55ee764399ae8df5d888b0719cee9d
Megan Gebhard <mgebhard1995@gmail.com>
2014-10-08 01:20:47 -0400

    Added time worked on a task method

--- project/Assets/Schedule/Schedule.cs
+++ project/Assets/Schedule/Schedule.cs
@@ -8,19 +8,24 @@ public class MITClass
 	public string task;
 	public string location;
 	public float minutesDue;
-	
-	public MITClass(string name, string number, string building, string assignment, float deadline)
+	public float minutesWorkedOn;
+	public float timeToComplete;
+	public bool taskComplete;
+
+	public MITClass(string name, string number, string building, 
+	                string assignment, float deadline, float workTime)
 	{
 		className = name;
 		courseNumber = number;
 		location = building;
 		task = assignment;
 		minutesDue = deadline;
+		timeToComplete = workTime;
+		taskComplete = false;
 	}
 
-	public void runClock (float minutesPassed)
+	public void RunClock (float minutesPassed)
 	{
-		Debug.Log("Counting down"); 
 		//I use the .1f multiplier to slow the time decay down
 		if(minutesDue - minutesPassed * .1f < 0)
 		{
@@ -31,9 +36,12 @@ public class MITClass
 		}
 	}
 
-	public void TaskExtension(float timeExtended)
+	public void TimeWorkedOnTask(float timeWorked)
 	{
-		minutesDue += timeExtended;
+		minutesWorkedOn += timeWorked;
+		if (minutesWorkedOn > timeToComplete) {
+			taskComplete = true;
+		}
 	}
 
 	public string toString() {
@@ -54,19 +62,19 @@ public class Schedule : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
-		taskOne = new MITClass("Algorithms", "6.006", "Green Building", "Test", 25);
-		taskTwo = new MITClass("Game Design", "6.073", "Stata", "Project 3", 30);
-		taskThree = new MITClass("Math for Computer Scientist", "6.042", "26-100", "Number Theorey Pset", 55);
+		taskOne = new MITClass("Algorithms", "6.006", "Green Building", "Test", 25, 10);
+		taskTwo = new MITClass("Game Design", "6.073", "Stata", "Project 3", 30, 12);
+		taskThree = new MITClass("Math for Computer Scientist", "6.042", "26-100", "Number Theorey Pset", 55, 20);
 
 		scheduleText = this.GetComponent<TextMesh> ();
 	}
-	
+
 	// Update is called once per frame
 	void Update () {
 		Debug.Log(taskOne.minutesDue);
-		taskOne.runClock (Time.deltaTime);
-		taskTwo.runClock (Time.deltaTime);
-		taskThree.runClock (Time.deltaTime);
+		taskOne.RunClock (Time.deltaTime);
+		taskTwo.RunClock (Time.deltaTime);
+		taskThree.RunClock (Time.deltaTime);
 		scheduleText.text = "Schedule: \n" + taskOne.toString() + "\n" + 
 			taskTwo.toString() + "\n" + taskThree.toString();
 	}
