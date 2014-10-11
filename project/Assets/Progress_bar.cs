661165f9e6df93a0c442bb9588b4df937814728e
Sen Chang <tonych@mit.edu>
2014-10-11 15:14:07 -0400

    better stats, progress bars, fixed fonts.

---
+++ project/Assets/Progress_bar.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+
+//To use:
+//In game, drag a progressbar prefab from assets to the place you want it to be.
+//in script, decalre a public gameobject variable.
+//In game, point the game object to the progressbar on the scene.
+//In script, change progressbar.val to change the filling. Change progressbar.text to change the text.
+
+public class Progress_bar : MonoBehaviour {
+	public float val;	//should be a percent value (0 -> 100%)
+	public string text; //this text is shown on the progress bar.
+	public GameObject background;
+	public GameObject foreground;
+	public GameObject text_gameobject;
+	private TextMesh stat_text_mesh;
+
+
+	void Start () {
+		stat_text_mesh = text_gameobject.GetComponent<TextMesh> ();
+	}
+
+	void Update() {
+		foreground.transform.localScale = new Vector3(val,1,1);
+		stat_text_mesh.text = text;
+
+
+	}
+	void hide(){
+		background.renderer.enabled = false;
+		foreground.renderer.enabled = false;
+		stat_text_mesh.renderer.enabled = false;
+	}
+
+	void show(){
+		background.renderer.enabled = true;
+		foreground.renderer.enabled = true;
+		stat_text_mesh.renderer.enabled = true;
+	}
+}
--- project/Assets/Stats/Stats_GUI.cs
+++ project/Assets/Stats/Stats_GUI.cs
@@ -7,7 +7,11 @@ public class Stats_GUI : MonoBehaviour {
 	public GameObject stat_background;
 	public GameObject stat_foreground;
 	public GameObject stat_text;
+	public int flash_freq;
 	private TextMesh stat_text_mesh;
+	private bool flashing = false;
+	private int flash_counter = 0;
+	public float flash_threshold = 0.1f;
 
 	void Start () {
 		stat_text_mesh = stat_text.GetComponent<TextMesh> ();
@@ -16,5 +20,35 @@ public class Stats_GUI : MonoBehaviour {
 	void Update() {
 		stat_foreground.transform.localScale = new Vector3(stat.val,1,1);
 		stat_text_mesh.text = stat.stat_name + ": " + System.String.Format("{0:P}",stat.val);
+
+		if (stat.val < flash_threshold) {
+			flashing = true;
+		}
+
+		if (stat.val > flash_threshold) {
+			flash_counter = 0;
+			flashing = false;
+		}
+
+		if (flashing & (flash_counter % flash_freq) < (flash_freq / 2)) {
+				hide (); 
+				flash_counter += 1;
+		} else if (flashing) {
+				show ();
+				flash_counter += 1;
+		} else {
+			show ();
+		}
+	}
+	void hide(){
+		stat_background.renderer.enabled = false;
+		stat_foreground.renderer.enabled = false;
+		stat_text_mesh.renderer.enabled = false;
+	}
+
+	void show(){
+		stat_background.renderer.enabled = true;
+		stat_foreground.renderer.enabled = true;
+		stat_text_mesh.renderer.enabled = true;
 	}
 }
